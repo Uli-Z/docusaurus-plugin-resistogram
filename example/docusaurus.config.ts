8d54a503aaecfe96c595a48064b880698d33bd0a
Uli-Z <58443149+Uli-Z@users.noreply.github.com>
2025-08-19 20:32:43 +0200

    ci: add GitHub Actions workflow for GitHub Pages deployment

--- example/docusaurus.config.ts
+++ example/docusaurus.config.ts
@@ -16,15 +16,17 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  // Set the production url of your site here
+  url: 'https://Uli-Z.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-plugin-resistogram/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Uli-Z', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-plugin-resistogram', // Usually your repo name.
+  deploymentBranch: 'gh-pages', // The branch to deploy to.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
